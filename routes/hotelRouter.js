1c892db4cd975810815867f6fee2a1a4fbd56aa3
Juanse <juanse.estrada@hotmail.com>
2018-09-26 23:01:39 -0500

    Prueba

--- routes/hotelRouter.js
+++ routes/hotelRouter.js
@@ -14,25 +14,6 @@ router.route("/").get(function (req, res) {
 
 });
 
-Hotel.find(function (err, result) {
-    result.forEach(function (hotel) {
-        var address = encodeURIComponent(hotel.address.replace(/"/g, ""));
-        
-        var url = "https://api.tomtom.com/search/2/geocode/"+address+".json?key=rGYSfp1LAs3MxKC65PHmwty97ZnZrX8G&limit=1&countrySet=IN";
-        request(url, function (error, response, body) {
-            console.log(url);
-            var data = JSON.parse(body);
-                if(data.results.length !== 0){
-                    Hotel.updateOne({ address: hotel.address },
-                        {
-                            $set: {
-                                latitude: data.results[0].position.lat,
-                                longitude: data.results[0].position.lon
-                            }
-                        });
-                    }
 
-        });
-    });
-});
+
 module.exports = router;
