be62977f96e1642216c4ce122b221a406dcd5cfb
Juanse <juanse.estrada@hotmail.com>
2018-09-28 22:17:13 -0500

    3rd Point done

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var bodyParser = require("body-parser");
 var HotelRouter = require("./routes/hotelRouter");
 var UserRouter = require("./routes/userRouter");
 
-//Comment
-//sdfsdfsdf
+
 mongoose.connect("mongodb://localhost/hotel", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
--- routes/hotelRouter.js
+++ routes/hotelRouter.js
@@ -5,14 +5,31 @@ var router = express.Router();
 var Hotel = require("../models/hotelModel");
 var request = require("request");
 
-router.route("/").get(function (req, res) {
+router.route("/").get(function (req, res,next) {
 
+    var query = req.query;
+    var range = query.range;
+    if (range) {
+        var maxlat = Number(query.lat) + Number(range);
+        var minlat = Number(query.lat) - Number(range);
+        var maxlon = Number(query.lon) + Number(range);
+        var minlon = Number(query.lon) - Number(range);
+        
+        Hotel.find({ latitude: { $gte: minlat, $lte: maxlat }, longitude: { $gte: minlon, $lte: maxlon } }, function(err,result){
+            res.json({ length: result.length,records: result});
+        });
+    }else{
+        next();
+    }
+});
+
+router.route("/").get(function (req, res) {
     var query = req.query;
     Hotel.find(query, function (err, result) {
         res.json({ length: result.length, records: result });
     });
-
 });
+
 
 
 
