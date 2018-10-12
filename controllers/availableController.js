45b6438c230aeaaf521634a283bae982937e8dad
Juan Estrada <jsestrada@uninorte.edu.co>
2018-10-12 15:36:22 -0500

    Reserva

--- controllers/availableController.js
+++ controllers/availableController.js
@@ -11,7 +11,7 @@ var query = req.query;
                 as: "reservation"
             }
         },
-        { $unwind: { path: "$reservation", "preserveNullAndEmptyArrays": true } },
+        { $unwind: { path: "$reservation"} },
         { $project: { "reservation": 1, "state": 1, "_id": 1 , "rooms":1, "startDate":1, "endDate": 1 } },
         {
             $match: {
@@ -30,34 +30,37 @@ var query = req.query;
                     }]
                 },
                 { $and: [{ 
-                    "reservation.startDate": { "$exists": false } 
+                    "reservation": { "$exists": false } 
                     }, 
                     { "state": query.state }] 
                 }]
             }
         }, {
             $group: {
-                _id: { state: "$state", startDate: query.startDate, endDate: query.endDate },
-                availableHotels: {
-                    $push: {
-                        hotelID: "$_id", totalRooms: "$rooms", availableRooms: {
-                            $sum: "$reservation.rooms_booked"
-                    }}
-                }
-                
+                _id: { hotelID: "$_id", totalRooms: "$rooms",state: "$state", startDate: query.startDate, endDate: query.endDate },
+                availableRooms: { $sum: "$reservation.rooms_booked"}
+
             }
-        } 
+        },
+        { $sort: { availableRooms: -1} } 
     ])
     .then(function (result) {
-        result[0].length = result[0].availableHotels.length;
-        result[0].availableHotels.forEach((hotel,index) => {
-                 var availables = hotel.totalRooms - hotel.availableRooms;
+        console.log(result)
+        result.forEach((hotel,index) => {
+            Hotel.find()
+            .then(function(hotel){
+                if (hotel._id !== result._id.hotelID) {
+                    var newHotel = { _id.hotelID: hotel._id }
+                    result.push()
+                }
+            });
+                 var availables = hotel._id.totalRooms - hotel.availableRooms;
                  hotel.availableRooms = availables;
                  if (availables <=0) {
                      result.splice(index,1);
                  }
              });
-        res.json({ length: result[0].length, results: result});
+        res.json({ length: result.length, results: result});
 
     });
 };
